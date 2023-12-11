a3c62271deafbe5bc4475ec782ae0ad8c71723d1
Evangelos Patsourakos <vaggpatsou@gmail.com>
2023-12-11 14:40:27 +0100

    Change the tempfilename to mkstemp.

--- archive/tests/libraries/auxiliary/test_logger.py
+++ archive/tests/libraries/auxiliary/test_logger.py
@@ -2,37 +2,38 @@ import unittest
 import logging
 import tempfile
 import os
+import sys
 from logger import Logger  # Assuming the Logger class is in logger.py
 
 class TestLogger(unittest.TestCase):
 
     def setUp(self):
-        # Create a name for a temporary file for logging without opening it
-        self.temp_log_file = tempfile.NamedTemporaryFile(delete=True)
-        self.log_file_name = self.temp_log_file.name
-        self.temp_log_file.close()  # Close the file, Logger will open it
+        # Create a temporary file for logging
+        fd, self.log_file_name = tempfile.mkstemp()
+        os.close(fd)  # Close the file descriptor
 
         # Initialize Logger with the temporary file name
         self.logger = Logger.get_instance(log_file=self.log_file_name, log_level=logging.DEBUG, file_logging=True, console_logging=False)
 
     def tearDown(self):
-        # Delete the temporary log file after testing if it exists
+        # Delete the temporary log file after testing
         if os.path.exists(self.log_file_name):
             os.remove(self.log_file_name)
-        Logger.instance = None
+        Logger.instance = None  # Reset the Logger instance for the next test
 
     def test_logging_to_file(self):
-        self.logger.log("Test message", level=logging.INFO)
+        test_message = "Test message"
+        self.logger.log(test_message, level=logging.INFO)
         with open(self.log_file_name, 'r') as f:
-            self.assertIn("INFO - Test message", f.read())
+            self.assertIn(test_message, f.read())
 
     def test_logging_with_different_levels(self):
         self.logger.log("Debug message", level=logging.DEBUG)
         self.logger.log("Warning message", level=logging.WARNING)
         with open(self.log_file_name, 'r') as f:
             log_content = f.read()
-            self.assertIn("DEBUG - Debug message", log_content)
-            self.assertIn("WARNING - Warning message", log_content)
+            self.assertIn("Debug message", log_content)
+            self.assertIn("Warning message", log_content)
 
     def test_logging_exception(self):
         try:
@@ -42,7 +43,7 @@ class TestLogger(unittest.TestCase):
                 self.logger.log_exception(e)
 
         with open(self.log_file_name, 'r') as f:
-            self.assertIn("ERROR - Exception occurred: Test exception", f.read())
+            self.assertIn("Test exception", f.read())
 
     def test_disabled_logging(self):
         disabled_logger = Logger.get_instance(disabled=True)
@@ -50,9 +51,21 @@ class TestLogger(unittest.TestCase):
         self.assertFalse(os.path.exists(self.log_file_name))
 
     def test_console_logging_only(self):
+        # Redirect stdout to capture console output
+        original_stdout = sys.stdout
+        sys.stdout = open(self.log_file_name, 'w')
+        
         console_logger = Logger.get_instance(log_file=None, console_logging=True, file_logging=False)
         console_logger.log("Console only message", level=logging.INFO)
-        self.assertFalse(os.path.exists(self.log_file_name))  # No file should be created
+        
+        sys.stdout.close()
+        sys.stdout = original_stdout
+
+        with open(self.log_file_name, 'r') as f:
+            self.assertIn("Console only message", f.read())
+
+        if os.path.exists(self.log_file_name):
+            os.remove(self.log_file_name)  # Clean up the temporary console log file
 
 if __name__ == '__main__':
     unittest.main()
